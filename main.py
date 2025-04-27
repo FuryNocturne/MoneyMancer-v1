02aae9bdae2dfddfbbb0705c3c66245d0db5ad69
FuryNocturne <jujulegal1@hotmail.fr>
2025-04-27 12:26:05 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from bot import scan_market
+from dashboard import show_dashboard
+
+if __name__ == "__main__":
+    print("\n🔥 PAR LE FEU DE NOS ÂMES ET LA FOUDRE DE NOS RÊVES 🔥")
+    print("⚡ MoneyMancer V7.0 - Full Combo Golden Soul lancé ! ⚡\n")
+
+    while True:
+        scan_market()
+        show_dashboard()
